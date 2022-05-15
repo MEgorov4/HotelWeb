30d4b1cf087291f42f811da6e67b954302835b05
MEgorov4 <47919432+MEgorov4@users.noreply.github.com>
2022-05-15 18:27:34 +0500

    Update ShowTable.php

--- ShowTable.php
+++ ShowTable.php
@@ -9,7 +9,6 @@ require_once "Connection/connect.php";
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-<!--    <link rel="stylesheet" type="text/css" href="ShowTable.php"> what is that?-->
     <link rel="stylesheet" type="text/css" href="css/main.css">
     <title>Document</title>
 </head>
